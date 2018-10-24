c6db43a1ad45109c3e94b9f5d9df1b7d1e9b8542
ynmwong <melodysweet@gmail.com>
2018-10-24 12:18:33 +0800

    font fixing

--- js/add-text.js
+++ js/add-text.js
@@ -16,15 +16,13 @@ class AddText extends PaintFunction{
         if(this.clicked==0){ //add text box
             this.arr.push(coord[0]);
             this.arr.push(coord[1]);
-            $('body').prepend(`<textarea id="addText" style="border:dotted 2px #000000; z-index:100; position:absolute; left:${coord[0]}px; top:${coord[1]}px; font-size:50px; font-family:${fontStyle}; color:${color}; background-color:transparent;">`);
+            $('body').prepend(`<textarea id="addText" style="border:dotted 2px #000000; z-index:100; position:absolute; left:${coord[0]}px; top:${coord[1]}px; font-size:50px; font-family:${fontStyle} ; color:${color}; background-color:transparent;">`);
             this.clicked=1;
         }else{ //place text
             var word=$('#addText').val();
             var x=this.arr.length-2;
             var y=this.arr.length-1;
-            console.log("place"+x,y)
-            this.contextReal.font = `50px ${fontStyle}`;
-            //this.contextReal.font = '50px arial';
+            this.contextReal.font = `50px ${fontStyle}` ;
             this.contextReal.globalAlpha = opacity;
             this.contextReal.fillStyle = color;
             this.contextReal.fillText(word,(this.arr[x]+4),(this.arr[y]+56));
@@ -43,9 +41,8 @@ class AddText extends PaintFunction{
 
         var word=$('#addText').val();
         $('#addText').remove();
-
         $('body').prepend(`<textarea id="addText"  style="border:dotted 2px #000000; z-index:100; position:absolute; left:${x}px; top:${y}px; font-size:50px; font-family:${fontStyle}; color:${color}; background-color:transparent;">${word}</textarea>`);
-        //comment
+ 
       });
     }
 
